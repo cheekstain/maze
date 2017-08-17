26c8d0d9f2ca1852f76471c5d9561896df62a393
ehobday <emma.c.hobday.19@dartmouth.edu>
2017-08-17 14:34:06 -0400

    Add tagging and tagging_strength
    The algorithm determining which way each avatar will move each time it's his
    turn is largely based on the idea of following other avatars' trails. Tagging
    is the trail, and the tag numbers get higher with each step that the avatar
    takes so that recent paths are higher priority than old ones.
    
    No avatar cares about the numbers of his own path.
    
    Avatars don't re-tag any space that has already been tagged unless an avatar
    is retracing his own steps, most likely due to a dead end.

--- mazestruct.c
+++ mazestruct.c
@@ -24,32 +24,40 @@ typedef struct mazesquare {
 	int south_wall;
 	int west_wall;
 	bool visited;
+	int tagged_by;
+	int tag_strength;
+	int *avatars_here;
 } mazesquare_t;
 
 /******************************** mazesquare_new ******************************/
-static mazesquare_t *mazesquare_new(void)
+static mazesquare_t *mazesquare_new(int num_avatars)
 {	
 	mazesquare_t *square = malloc(sizeof(mazesquare_t));
+
+	square->avatars_here = calloc(num_avatars, sizeof(int));
 
 	square->north_wall = -1;
 	square->south_wall = -1;
 	square->east_wall = -1;
 	square->west_wall = -1;
 	square->visited = false;
+	square->tagged_by = -1;
+	square->tag_strength =-1;
 }
 
 /******************************** maze_new ************************************/
-maze_t *maze_new(int x_size, int y_size)
+maze_t *maze_new(int width, int height, int num_avatars)
 {
 	//malloc for the maze itself
 	maze_t *maze = malloc(sizeof(maze_t));
 
 	//create an empty array
+	maze->array = calloc(width * height, sizeof(mazesquare_t));
 
 	//fill it by creating an empty array for each coordinate
-	for (int x = 0; x < x_size; x++) {
-		for (int y = 0; y < y_size; y++) {
-			maze->array[x][y] = mazesquare_new();
+	for (int x = 0; x < width; x++) {
+		for (int y = 0; y < height; y++) {
+			maze->array[x][y] = mazesquare_new(num_avatars);
 		}
 	}
 }
@@ -143,18 +151,26 @@ bool get_visited(int x_coord, int y_coord)
 	return square->visited;
 }
 
-int get_first_visitor(int x_coord, y_coord)
+/******************************** get_visited *********************************/
+int get_tagged_by(int x_coord, y_coord)
 {
-	mazesquare_t *square get_square_at_coords(x_coord, y_coord);
-	return square->first_visitor;
+	mazesquare_t *square = get_square_at_coords(x_coord, y_coord);
+	return square->tagged_by;
+}
+
+int get_tag_strength()
+{
+	mazesquare_t *square = get_square_at_coords(x_coord, y_coord);
+	return square->tagged_by;
 }
 
 /******************************** set_visited *********************************/
-void set_visited(maze_t *maze, int x_coord, int y_coord, int visitor)
+void set_visited(maze_t *maze, int x_coord, int y_coord, int visitor, int tag_strength)
 {
-	mazesquare_t *square = maze->array[x_coord][y_coord];
+	mazesquare_t *square = get_square_at_coords(x_coord, y_coord);
 	square->visited = true;
-	square->first_visitor = visitor;
+	square->tagged_by = visitor;
+	square->tag_strength = tag_strength;
 }
 
 /******************************** maze_delete *********************************/
@@ -163,28 +179,69 @@ void maze_delte(maze_t *maze)
 	//go through the array and delete each mazesquare struct
 	for (int x = 0; x < x_size; x++) {
 		for (int y = 0; y < y_size; y++) {
-			mazesquare_t maze->array[x][y] = mazesquare_new();
+			mazesquare_t *square = get_square_at_coords(x, y);
+			free(square);
 		}
 	}
+
 	//delete the array
+	free(maze->array);
 	//delte the maze itself
+	free(maze);
 }
 
+/******************************** draw_maze ***********************************/
+void draw_maze(maze_t *maze)
+{
+	//go through the array and delete each mazesquare struct
+	for (int y = 0; y < height; y++) {
+		draw_floor();
+		printf("\n");
+
+		for (int x = 0; x < width; x++) {
+			
+		}
+	}
+}
+
+void draw_floor()
+{
+	printf(" _____");
+}
+
+void draw_no_floor()
+{
+	printf("      ");
+}
+
+/******************************** draw_maze ***********************************/
+/*
+ * Draw only the left side and bottom of each square to avoid redrawing walls.
+ */
+static void draw_square(mazesquare_t *square)
+{
+
+}
+
+/* Maze UI will look like this:
+ _____ _____ _____
+|                 |
+|  1              |
+|      _____      |
+|     |           |
+|     |           |
+|     |_____      |
+|           |     |
+|           |  0  |
+|_____ _____|_____|
 
 
 
 
+*/
 
 
-
-
-
-
-
-
-
-
-
+For the first row, draw a 
 
 
 
