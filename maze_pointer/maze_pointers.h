f828e60046951e00408197e4a5c4ce89d87e65a4
Bill Tang <bill.y.tang.20@dartmouth.edu>
2017-08-21 19:09:52 -0400

    Update maze_pointers.h

--- maze_pointer/maze_pointers.h
+++ maze_pointer/maze_pointers.h
@@ -16,34 +16,34 @@
 
 typedef struct maze_data_pointer_struct maze_pointers_t;
 
-maze_pointers_t* maze_pointers_new(const char* hostname, 
-                         const int maze_port,
-                         const char* filename,
-                         const int avatar_id,
-                         const maze_t* maze,
-                         const lastmove_t* lastmove,
-                         const counters_t* follow_list);
+maze_pointers_t* maze_pointers_new(char* hostname, 
+                         int maze_port,
+                         char* filename,
+                         int avatar_id,
+                         maze_t* maze,
+                         lastmove_t* lastmove,
+                         counters_t* follow_list);
 /*
  * Getter Funcs
  * at no point are ANY of these pointers to be modified. These are for
  * information purposes ONLY.
  */
-const char* get_hostname(maze_pointers_t *ptr);
-const int get_maze_port(maze_pointers_t *ptr);
-const char* get_filename(maze_pointers_t *ptr);
-const int get_avatar_id(maze_pointers_t *ptr);
-const maze_t* get_maze(maze_pointers_t *ptr);
-const lastmove_t* get_lastmove(maze_pointers_t *ptr);
-const counters_t* get_follow_list(maze_pointers_t *ptr);
+char* get_hostname(maze_pointers_t *ptr);
+int get_maze_port(maze_pointers_t *ptr);
+char* get_filename(maze_pointers_t *ptr);
+int get_avatar_id(maze_pointers_t *ptr);
+maze_t* get_maze(maze_pointers_t *ptr);
+lastmove_t* get_lastmove(maze_pointers_t *ptr);
+counters_t* get_follow_list(maze_pointers_t *ptr);
 
 /*
  * Setter Methods - use these to change, if necessary.
  */
-void set_hostname(maze_pointers_t *ptr, const char* hostname);
-void set_maze_port(maze_pointers_t *ptr, const int maze_port);
-void set_filename(maze_pointers_t *ptr, const char* filename);
-void set_avatar_id(maze_pointers_t *ptr, const int avatar_id);
-void set_maze(maze_pointers_t *ptr, const maze_t* maze);
+void set_hostname(maze_pointers_t *ptr, char* hostname);
+void set_maze_port(maze_pointers_t *ptr, int maze_port);
+void set_filename(maze_pointers_t *ptr, char* filename);
+void set_avatar_id(maze_pointers_t *ptr, int avatar_id);
+void set_maze(maze_pointers_t *ptr, maze_t* maze);
 void set_lastmove(maze_pointers_t *ptr, lastmove_t* lastmove);
 void set_follow_list(maze_pointers_t *ptr, counters_t* follow_list);
 
