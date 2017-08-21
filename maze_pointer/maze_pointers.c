29456d5f59d422d44cf30a7316a115d204bb2d7e
Bill Tang <bill.y.tang.20@dartmouth.edu>
2017-08-21 19:05:37 -0400

    Update maze_pointers.c

--- maze_pointer/maze_pointers.c
+++ maze_pointer/maze_pointers.c
@@ -38,13 +38,13 @@ maze_pointers_t* maze_pointers_new(const char* hostname,
                          const lastmove_t* lastmove,
                          const counters_t* follow_list){
   maze_pointers_t* tmp = allocate(sizeof(pointers_t));
-  tmp.hostname = strdup(hostname);
-  tmp.maze_port = maze_port;
-  tmp.filename = strdup(filename);
-  tmp.avatar_id = avatar_id;
-  tmp.maze = maze;
-  tmp.lastmove = lastmove;
-  tmp.follow_list = follow_list);
+  tmp->hostname = strdup(hostname);
+  tmp->maze_port = maze_port;
+  tmp->filename = strdup(filename);
+  tmp->avatar_id = avatar_id;
+  tmp->maze = maze;
+  tmp->lastmove = lastmove;
+  tmp->follow_list = follow_list);
   return tmp;
 }
 
@@ -53,62 +53,62 @@ maze_pointers_t* maze_pointers_new(const char* hostname,
  */
 
 const char* get_hostname(maze_pointers_t *ptr){
-  return ptr.hostname;
+  return ptr->hostname;
 } 
 
 const int get_maze_port(maze_pointers_t *ptr){
-  return ptr.maze_port;
+  return ptr->maze_port;
 }
 
 const char* get_filename(maze_pointers_t *ptr){
-  return ptr.filename;
+  return ptr->filename;
 }
 
 const int get_avatar_id(maze_pointers_t *ptr){
-  return ptr.avatar_id;
+  return ptr->avatar_id;
 }
 
 const maze_t* get_maze(maze_pointers_t *ptr){
-  return ptr.maze;
+  return ptr->maze;
 }
 
 const lastmove_t* get_lastmove(maze_pointers_t *ptr){
-  return ptr.lastmove;
+  return ptr->lastmove;
 }
 
 const counters_t* get_follow_list(maze_pointers_t *ptr){
-  return ptr.follow_list;
+  return ptr->follow_list;
 }
 
 /*
  * Setter Methods
  */
 void set_hostname(maze_pointers_t *ptr, const char* hostname){
-  ptr.hostname = strdup(hostname);
+  ptr->hostname = strdup(hostname);
 } 
 
 void set_maze_port(maze_pointers_t *ptr, const int maze_port){
-  ptr.maze_port = maze_port;
+  ptr->maze_port = maze_port;
 }
 
 void set_filename(maze_pointers_t *ptr, const char* filename){
-  ptr.filename = strdup(filename);
+  ptr->filename = strdup(filename);
 }
 
 void set_avatar_id(maze_pointers_t *ptr, const int avatar_id){
-  ptr.avatar_id = avatar_id;
+  ptr->avatar_id = avatar_id;
 }
 
 void set_maze(maze_pointers_t *ptr, const maze_t* maze){
-  ptr.maze = maze;
+  ptr->maze = maze;
 }
 
 void set_lastmove(maze_pointers_t *ptr, lastmove_t* lastmove){
-  ptr.lastmove = lastmove;
+  ptr->lastmove = lastmove;
 }
 
 void set_follow_list(maze_pointers_t *ptr, counters_t* follow_list){
-  ptr.follow_list = follow_list;
+  ptr->follow_list = follow_list;
 }
 
 /*
