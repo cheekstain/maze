b8c200740b7b9823d2c773c63d68f2a45fb1eb71
Bill Tang <bill.y.tang.20@dartmouth.edu>
2017-08-21 18:58:21 -0400

    Update maze_pointers.h

--- maze_pointer/maze_pointers.h
+++ maze_pointer/maze_pointers.h
@@ -12,6 +12,7 @@
 #include "counters.h"
 #include "memory.h"
 #include <string.h>
+#include <maze_pointers.h>
 #include <../mazestruct/mazestruct.h>
 
 maze_pointers_t* maze_pointers_new(const char* hostname, 
